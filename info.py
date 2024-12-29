8fc2a496975ca08957e9014ee5400870b94cbf7f
Itsharshu143 <116162890+Itsharshu143@users.noreply.github.com>
2024-12-29 10:44:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://5l8pi:HejeyYhU62GPAzBO@cluster0.7plo7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Harshu')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_file')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002458696224'))
 QR_CODE = environ.get('QR_CODE', 'https://envs.sh/Js4.jpg')
 START_IMG = environ.get('START_IMG', 'https://envs.sh/Jsi.jpg')
